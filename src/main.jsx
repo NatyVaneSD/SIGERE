a8bd95096b40dfaa3d4e28571ad72b2aadfde183
Natalia Macedo <natalia.macedo@discente.ufra.edu.br>
2025-09-04 16:49:53 -0300

    feat: Adicionar tela de login e estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Login from "./components/Login.jsx"; 
+import './index.css'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login /> {/* Renderize o componente de Login aqui */}
+  </React.StrictMode>,
+);
